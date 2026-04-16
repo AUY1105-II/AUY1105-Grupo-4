bedb730080dd888cab5ee01a9796f8d69959bbc0
mafernandezz <ma.fernandezz@duocuc.cl>
2026-04-15 20:00:21 -0400

    UPDATE: Agregar modulos de EC2 y SG en main.tf y poblado de CHANGELOG.md

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Registro de Cambios (Changelog)
+
+Todas las novedades significativas de este proyecto se documentarán en este archivo.
+
+## 15-04-26
+
+### Añadido
+
+- Creación de archivos iniciales:
+  - CHANGELOG.md
+  - main.tf
+  - providers.tf
+  - README.md
+  - variables.ft
--- main.tf
+++ main.tf
@@ -18,3 +18,30 @@ module "vpc" {
   }
 }
 
+
+module "web_server_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "web-server"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = "vpc-12345678"
+
+  ingress_cidr_blocks = ["10.10.0.0/16"]
+}
+
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "single-instance"
+
+  instance_type = "t2.micro"
+  key_name      = "user1"
+  monitoring    = true
+  subnet_id     = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
